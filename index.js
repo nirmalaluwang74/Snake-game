29325a2eeb5d694b141c9723f2c3d28708f27822
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-07-31 20:56:14 +0530

    snake-game

---
+++ index.js
@@ -0,0 +1,18 @@
+const startBtn = document.getElementById("start")
+const grid = document.querySelector('.grid')
+const score = document.getElementById('score')
+let squares = []
+let currentSnake = [0,1,2]
+
+function createGrid() {
+    for (let i = 0; i < 100; i++ ) {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        grid.appendChild(square)
+        squares.push(square)
+    }
+}
+createGrid()
+
+
+currentSnake.forEach(index => squares[index].classList.add('snake'))
